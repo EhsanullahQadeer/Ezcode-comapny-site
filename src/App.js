881f8741ef817fc470be356adf2144d6c43cb739
Husnain-Shakil <husnainshakil123@gmail.com>
2024-03-03 04:56:44 +0500

    bg-color-added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,25 @@
+import { Header } from "./components/global/Header";
+
 function App() {
   return (
-    <div className="App">
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
-    </div>
+    <>
+      <div
+        style={{
+          height: "100svh",
+        }}
+      >
+        <div
+          style={{
+            maxWidth: "80%",
+            margin: "auto",
+            padding: "30px",
+          }}
+        >
+          <Header />
+          <h1 className="text-3xl font-bold underline">Hello world!</h1>
+        </div>
+      </div>
+    </>
   );
 }
 
